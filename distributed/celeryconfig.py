979c821f96786505b16b91bc5228a5f4a697acf8
Fyodor <fygrave@gmail.com>
2013-03-16 21:54:10 +0800

    too many erries

--- distributed/celeryconfig.py
+++ distributed/celeryconfig.py
@@ -2,8 +2,9 @@
 
 BROKER_URL = 'localhost'
 CELERY_TASK_SERIALIZER='json'
-CELERY_RESULT_BACKEND = 'amqp'
-CELERY_RESULT_SERIALIZER = 'json'
+# lots of   queues
+#CELERY_RESULT_BACKEND = 'amqp'
+#CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_EXCHANGE='dnscelerytask'
 #CELERY_RESULT_EXCHANGE = 'dnscelery'
 CELERY_TIMEZONE = 'Asia/Taipei'
